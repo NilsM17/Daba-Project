5dc83f35459793a7577ee6429e2756ae8b392f4a
NilsM17 <1059147@bkrnet.de>
2025-01-15 12:49:32 +0100

    Add button to navigate to Pfleger page and improve layout structure

--- src/app/Pfleger/page.tsx
+++ src/app/Pfleger/page.tsx
@@ -1,5 +1,5 @@
 import { prisma } from '@/db';
-import { Table, TableCell, TableHead, TableRow } from '@mui/material';
+import { Button, Table, TableCell, TableHead, TableRow } from '@mui/material';
 import React from 'react'
 
 async function Pfleger() {
@@ -20,28 +20,33 @@ async function Pfleger() {
     
       const pfleger = await prisma.pfleger.findMany();
       const orte = await prisma.orte.findMany();
-    return (
-   
+      
+return (
+  <div>
+    <Button sx={{ textTransform: "capitalize", marginRight: 1 }}  variant="contained" color="primary" href="/Pfleger" >
+      Pfleger
+    </Button>
     <Table>
-        <TableHead >
-          <TableCell sx={{ color: "white" }}>Lastname</TableCell>
-          <TableCell sx={{ color: "white" }}>Firstname</TableCell>
-          <TableCell sx={{ color: "white" }}>Ort</TableCell>
-          <TableCell sx={{ color: "white" }}>Phonenumber</TableCell>
-        </TableHead>
-        {pfleger.map((p) => {
-          const ort = orte.find((o) => o.PLZ === p.PLZ);
-          return (
-            <TableRow key={p.id}>
-              <TableCell>{p.LastName}</TableCell>
-              <TableCell>{p.FirstName}</TableCell>
-              <TableCell>{ort ? ort.Name : "Unknown"}</TableCell>
-              <TableCell>{p.Phonenumber}</TableCell>
-            </TableRow>
-          );
-        })}
-      </Table>
-  )
+      <TableHead >
+        <TableCell sx={{ color: "white" }}>Lastname</TableCell>
+        <TableCell sx={{ color: "white" }}>Firstname</TableCell>
+        <TableCell sx={{ color: "white" }}>Ort</TableCell>
+        <TableCell sx={{ color: "white" }}>Phonenumber</TableCell>
+      </TableHead>
+      {pfleger.map((p) => {
+        const ort = orte.find((o) => o.PLZ === p.PLZ);
+        return (
+          <TableRow key={p.id}>
+            <TableCell>{p.LastName}</TableCell>
+            <TableCell>{p.FirstName}</TableCell>
+            <TableCell>{ort ? ort.Name : "Unknown"}</TableCell>
+            <TableCell>{p.Phonenumber}</TableCell>
+          </TableRow>
+        );
+      })}
+    </Table>
+  </div>
+)
 }
 
 export default Pfleger
